09035be1497e2f47bc6e6d79d5093b3c66e71257
Stef Siekman <s.siekman@gmail.com>
2018-12-05 09:25:51 +0100

    Improve printing of 5.2

--- 5/second.py
+++ 5/second.py
@@ -1,18 +1,20 @@
 def forLetter(input):
     res = []
     for i in range(ord('a'), ord('z') + 1):
-        print(chr(i))
+        print(chr(i).upper(), end="")
         letter = chr(i)
         line = input[:]
         line = line.replace(letter.lower(), "")
         line = line.replace(letter.upper(), "")
-        res.append(solve(line))
+        r = solve(line)
+        res.append(r)
+        print(f" {r}")
 
+    print("\nFound the minimum to be:")
     print(min(res))
 
 def solve(line):
     startFrom = 0
-    prints = 0
     while True:
         found = False
         last = None
@@ -22,7 +24,7 @@ def solve(line):
             if (c.lower() == last and c.isupper()) or (c.upper() == last and c.islower()):
                 newLine = line[:i-1] + line[i+1:]
                 line = newLine
-                startFrom = i - 10
+                startFrom = i - 2
                 if startFrom < 0:
                     startFrom = 0
                 found = True
@@ -30,12 +32,8 @@ def solve(line):
 
             last = c
 
-        if len(line) % 1000 == 0:
-            print(f"\r{len(line)}", end="")
-
         if not found:
             break
-    print(f"\r{len(line)}")
     return len(line)
 
 
