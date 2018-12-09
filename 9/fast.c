1962e86f0abbe5b3b26d5138c900f38bc1b8d7fe
Stef Siekman <s.siekman@gmail.com>
2018-12-09 13:11:42 +0100

    Beginning of C implementation

---
+++ 9/fast.c
@@ -0,0 +1,17 @@
+#include "fast.h"
+
+struct Marbel {
+    unsigned long number;
+    Marbel* left;
+    Marbel* right;
+};
+
+unsigned long solve(int players, unsigned long marbels) {
+    return 0;
+}
+
+int main(int argc, char** argv) {
+    unsigned long result = solve(5, 25);
+    printf("Result is: %lu\n", result);
+    return 0;
+}
---
+++ 9/fast.h
@@ -0,0 +1,11 @@
+#ifndef FAST_H
+#define FAST_H
+
+#include <stdio.h>
+#include <stddef.h>
+
+typedef struct marbel Marbel;
+
+unsigned long solve(int players, unsigned long marbels); 
+
+#endif
