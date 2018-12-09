d2bda5939a8ea59dae1e620796134a290e98ee80
Stef Siekman <s.siekman@gmail.com>
2018-12-09 13:20:05 +0100

    Implement Marbel constructor

--- 9/fast.c
+++ 9/fast.c
@@ -1,10 +1,25 @@
 #include "fast.h"
 
-struct Marbel {
+struct marbel {
     unsigned long number;
     Marbel* left;
     Marbel* right;
 };
+
+Marbel* create_marbel(unsigned long number, Marbel* left, Marbel* right) {
+    // All properties are immediately set, no need to clear
+    Marbel* marbel = malloc(sizeof(Marbel));
+
+    marbel->number = number;
+    marbel->left = left;
+    marbel->right = right;
+
+    // Set left and right to self if they're NULL
+    if (!left) { marbel->left = marbel; }
+    if (!right) { marbel->right = marbel; }
+
+    return marbel;
+}
 
 unsigned long solve(int players, unsigned long marbels) {
     return 0;
--- 9/fast.h
+++ 9/fast.h
@@ -2,9 +2,12 @@
 #define FAST_H
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <stddef.h>
 
 typedef struct marbel Marbel;
+
+Marbel* create_marbel(unsigned long number, Marbel* left, Marbel* right);
 
 unsigned long solve(int players, unsigned long marbels); 
 
